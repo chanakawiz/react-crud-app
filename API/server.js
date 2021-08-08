4703143366838582708ff4858b48ff0449a0c4a4
chanaka sampath <55937326+chanakawiz@users.noreply.github.com>
2021-08-08 16:11:39 +0530

    server and database initialized

---
+++ API/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = 4000;
+const cors = require("cors");
+
+
+//import mongo db
+const mongoose = require('mongoose');
+//import the db js file
+const config = require('./Db.js');
+
+//connect to the database
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+  () => { console.log('Database is connected') }, err => { console.log('cannot connect to the database' + err) }
+);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(PORT, function () {
+  console.log("server is running on port:", PORT);
+});
